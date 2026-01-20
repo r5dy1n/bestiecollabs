3c8f9af845d5d989218bb56e83ded9dd6aa247ec
Raymond Yan <raymondyan217@gmail.com>
2026-01-20 13:45:53 -0800

    fix: migration

--- database/migrations/2026_01_20_060344_add_user_type_to_users_table.php
+++ database/migrations/2026_01_20_060344_add_user_type_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('user_type')->after('email');
+            $table->string('user_type')->default('creator')->after('email');
         });
     }
 
